9bb6b30b1e76c941b7b64145eef75ec28640e2d5
Swamii <anelander@gmail.com>
2013-01-08 09:27:22 +0100

    double connect()

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -10,17 +10,7 @@ public class ClientMain {
 	public static void main(String[] args) {
 		Client client = new Client();
 		client.startProgram();
-
-		try {
-			client.connect("localhost", 5000);
-			client.chatting();
-		} catch (UnknownHostException e) {
-			System.err.println("Unknown host exception: " + e.getMessage());
-			System.exit(2);
-		} catch (IOException e) {
-			System.err.println("Error: " + e.getMessage());
-			System.exit(1);
-		}
+		client.chatting();
 	}
 
 }
